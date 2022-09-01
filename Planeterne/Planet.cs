65ab02670164de9c39b2bdfb6390b44ced28981a
Tue Munch Madsen <tuepvm@gmail.com>
2022-09-01 11:02:02 +0200

    Add project files.

---
+++ Planeterne/Planet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planeterne
+{
+    internal class Planet
+    {
+        //Instance variables
+        private string name;
+        private int diameter;
+        private int temperature;
+
+        //Makes it so we can get the variables in the main method
+        public string Name { get { return name; } }
+        public int Temperature { get { return temperature; } }
+        public int Diameter { get { return diameter; } }
+
+
+
+        //Konstruckter
+        public Planet(string name, int diameter, int temperature)
+        {
+            this.name = name;
+            this.diameter = diameter;
+            this.temperature = temperature;
+        }
+    }
+}
---
+++ Planeterne/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Planeterne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crates a list of the datatype Planet
+            List<Planet> planets = new List<Planet>();
+            
+            //Creates the planets as objekts
+            Planet mercury = new Planet("Mercury",4879,167);
+            Planet venus = new Planet("Venus", 12104, 464);
+            Planet earth = new Planet("Earth", 12756, 15);
+            Planet mars = new Planet("Mars", 6792, -65);
+            Planet jupiter = new Planet("Jupiter", 142984, -110);
+            Planet saturn = new Planet("Saturn", 120536, -140);
+            Planet uranus = new Planet("Uranus", 51118, -195);
+            Planet neptune = new Planet("Neptune", 49528, -200);
+            Planet pluto = new Planet("Pluto", 2370, -225);
+
+            //Adds the planets to the list
+            planets.Add(mercury);
+            planets.Add(earth);
+            planets.Add(mars);
+            planets.Add(jupiter);
+            planets.Add(saturn);
+            planets.Add(uranus);
+            planets.Add(neptune);
+            planets.Add(pluto);
+
+            //Writes out the planets list
+            Console.WriteLine("Opg 1");
+            foreach (Planet p in planets)
+            {
+                Console.WriteLine(p.Name);
+            }
+            //inserts venus on the lists index 1
+            planets.Insert(1,venus);
+
+            //Writes out planets list
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Opg 2");
+            foreach (Planet p in planets)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //removes pluto from the list 
+            planets.Remove(pluto);
+
+
+            //writes out the planets list
+            Console.WriteLine("\r\n");
+            Console.WriteLine("opg 3");
+            foreach (Planet p in planets)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //Adds pluto back
+            planets.Add(pluto);
+
+            //Writes out the planets list
+            Console.WriteLine("\r\n");
+            Console.WriteLine("opg 4");
+            foreach (Planet p in planets)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //writes out how mant objekts there are in the list planets
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Opg 5");
+            Console.WriteLine(planets.Count);
+
+
+            //creates a new list
+            List<Planet> planetTemperature = new List<Planet>();
+
+            //add all objekts with a temperature less then 0 to the planetTemperature list
+            foreach (Planet p in planets)
+            {
+                if (p.Temperature < 0)
+                {
+                    planetTemperature.Add(p);
+                }
+            }
+
+            //Writes out the planetTemperature list
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Opg 6");
+            foreach (Planet p in planetTemperature)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //creates a new list again
+            List<Planet> planetsDiameter = new List<Planet>();
+
+            //adds all objekts from the list planets with a diameter bigget then 10000 and less then 50000 to the new list planetsDiameter
+            foreach (Planet p in planets)
+            {
+                if (p.Diameter > 10000 && p.Diameter < 50000)
+                {
+                    planetsDiameter.Add(p);
+                }
+            }
+
+            //writes out the planetsDiameter list
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Opg 7");
+            foreach (Planet p in planetsDiameter)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            //Clears the planets list and writes it out
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Opg 8");
+            planets.Clear();
+            foreach (Planet p in planets)
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+}
